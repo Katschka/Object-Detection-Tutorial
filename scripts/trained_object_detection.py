84675b210159057dee6a78254bc23bc01403e56a
Katschka <91552083+Katschka@users.noreply.github.com>
2021-11-04 13:04:08 +0100

    finalized commands file

--- scripts/trained_object_detection.py
+++ scripts/trained_object_detection.py
@@ -1,4 +1,4 @@
-""" usage: trained_object_detection.py [-h] [-f FILETYPE] [-i IMAGE_PATH] [-l LABEL_PATH] [-m MODEL_PATH]
+""" usage: trained_object_detection.py [-h] [-f FILETYPE] [-i IMAGE_PATH] [-l LABEL_PATH] [-m MODEL_PATH] [-o OUTPUT_PATH] [-s SUPPRESS_SHOWING]
 
 Partition dataset of images into training and testing sets
 
@@ -10,6 +10,10 @@ optional arguments:
                  Path to the folder containing the .pbtxt-file containing your (converted) labels
   -m MODEL_PATH, --MODEL_PATH MODEL_PATH
                  Path to the exported, trained model with the .pb file extension
+  -o OUTPUT_PATH, --OUTPUT_PATH OUTPUT_PATH
+  		  Path to save the detections
+  -s SUPPRESS_SHOWING, --SUPPRESS_SHOWING SUPPRESS_SHOWING
+  		  Set this boolean (true/false) to (not) show the detections
 """
 
 
@@ -33,10 +37,10 @@ from PIL import Image
 
 
 # Prerequisites and preperations
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'    # Suppress TensorFlow logging (1)
-tf.get_logger().setLevel('ERROR')           # Suppress TensorFlow logging (2)
-matplotlib.use('qt5agg')
-warnings.filterwarnings('ignore')   # Suppress Matplotlib warnings
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"    # Suppress TensorFlow logging (1)
+tf.get_logger().setLevel("ERROR")           # Suppress TensorFlow logging (2)
+matplotlib.use("qt5agg")
+warnings.filterwarnings("ignore")   # Suppress Matplotlib warnings
 
 
 
@@ -47,7 +51,7 @@ parser.add_argument(
   "-i", "--IMAGE_PATH",
   help="Path to the folder containing the images to detect objects in are stored. If not specified, the path from the tutorial will be used",
   type=str,
-  default="/home/$USER/TensorFlow/workspace/training_demo/images/detect"
+  default="/home/$USER/TensorFlow/workspaces/training_demo/images/images_to_detect"
   )
 
 parser.add_argument(
@@ -61,15 +65,29 @@ parser.add_argument(
   "-l", "--LABEL_PATH",
   help="Path to the folder containing the label file with .pbtxt-extension. If not specified, the path from the tutorial will be used",
   type=str,
-  default="/home/$USER/TensorFlow/workspace/training_demo/annotations/label_map.pbtxt"
+  default="/home/$USER/TensorFlow/workspaces/training_demo/annotations/label_map.pbtxt"
 )
 
 parser.add_argument(
   "-m", "--MODEL_PATH",
   help="Path to the folder containing the trained and exported model file with .pb-extension. If not specified, the path from the tutorial will be used",
   type=str,
-  default="/home/$USER/TensorFlow/workspace/training_demo/exported-models/demo_model/saved_model"
+  default="/home/$USER/TensorFlow/workspaces/training_demo/exported-models/ssd_resnet50_v1_fpn_640x640_coco17_tpu-8/saved_model"
 )
+
+parser.add_argument(
+  "-o", "--OUTPUT_PATH",
+  help="If defined, the detections will be saved at this folder. If undefined, the detections will be shown for every input picture, but will not be saved automatically",
+  type=str,
+  default="none"
+)
+
+parser.add_argument(
+  "-s", "--SUPPRESS_SHOWING",
+  help="Set this flag if you do not want or need to see the detecions pop up in seperate windows. If this flag is not set, detections will show",
+  action="store_false"
+)
+
 args = parser.parse_args()
 
 
@@ -78,19 +96,20 @@ args = parser.parse_args()
 IMAGE_PATH = args.IMAGE_PATH.replace("/home/$USER", str(pathlib.Path.home()))
 LABEL_PATH = args.LABEL_PATH.replace("/home/$USER", str(pathlib.Path.home()))
 MODEL_PATH = args.MODEL_PATH.replace("/home/$USER", str(pathlib.Path.home()))
+OUTPUT_PATH = args.OUTPUT_PATH.replace("/home/$USER", str(pathlib.Path.home()))
 FILETYPE = args.FILETYPE.replace(".", "")
 
 
 # Load the trained model:
-print('Loading model...', end='')
+print("Loading model...", end="")
 start_time = time.time()
 
 model = tf.saved_model.load(MODEL_PATH)
-#model = model.signatures['serving_default']
+#model = model.signatures["serving_default"]
 
 end_time = time.time()
 elapsed_time = end_time - start_time
-print('Done! Took {} seconds'.format(elapsed_time))
+print("Done! Took {} seconds".format(elapsed_time))
 
 
 
@@ -107,32 +126,33 @@ images = sorted(list(pathlib.Path(IMAGE_PATH).glob("*." + FILETYPE)))
 # Start the detection
 for image in images:
   
-  print('Running inference for {}... '.format(image))
-  image_name = str(image).rsplit("/", 1)[-1]
+  print("Running inference for {}... ".format(image))
+  start_time = time.time()
+  image_name = (str(image).rsplit("/", 1)[-1]).replace(("." + FILETYPE),"")
   # Convert image to NN-usable array
   np_image = np.array(Image.open(image))
   # Convert the image to a "tensor" which TF can use
   input_tensor = tf.convert_to_tensor(np_image)
-  # By Definition: The model expects an batch of images, so add an dimension (=axis) as part of it's inputs:
+  # By Definition: The model expects an batch of images, so add an dimension (=axis) as part of it"s inputs:
   input_tensor = input_tensor[tf.newaxis,...]
   # Run the detection
   detections = model(input_tensor)
   # Get the relevant outputs (num_detections) and remove the no more needed batch dimension
-  num_detections = int(detections.pop('num_detections'))
+  num_detections = int(detections.pop("num_detections"))
   detections = {key: value[0, :num_detections].numpy()
                   for key, value in detections.items()}
-  detections['num_detections'] = num_detections
+  detections["num_detections"] = num_detections
 
   # detection_classes should be ints.
-  detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+  detections["detection_classes"] = detections["detection_classes"].astype(np.int64)
   
   image_with_detections = np_image.copy()
 
   vis_utils.visualize_boxes_and_labels_on_image_array(
     image_with_detections,
-    detections['detection_boxes'],
-    detections['detection_classes'],
-    detections['detection_scores'],
+    detections["detection_boxes"],
+    detections["detection_classes"],
+    detections["detection_scores"],
     category_index,
     use_normalized_coordinates=True,
     max_boxes_to_draw=200,
@@ -140,19 +160,27 @@ for image in images:
     agnostic_mode=False)
 
 
-  boxes = detections['detection_boxes']
-  classes = detections['detection_classes'],
+  boxes = detections["detection_boxes"]
+  classes = detections["detection_classes"],
   max_boxes_to_draw = boxes.shape[0]
-  scores = detections['detection_scores']
+  scores = detections["detection_scores"]
   min_score_thresh=0.5
   for i in range(min(max_boxes_to_draw, boxes.shape[0])):
     if scores is None or scores[i] > min_score_thresh:
       # boxes[i] is the box which will be drawn
       print ("This is a ", category_index[classes[0][i]]["name"], " at ", boxes[i])
 
-  plt.figure(image_name)
-  plt.imshow(image_with_detections)
-  print('Done')
+  if args.SUPPRESS_SHOWING:
+    plt.figure(image_name)
+    plt.imshow(image_with_detections)
+
+  if OUTPUT_PATH != "none":
+    file_name = OUTPUT_PATH + "/" + image_name + ".jpg"
+    plt.imsave(file_name, image_with_detections)
+  
+  end_time = time.time()
+  elapsed_time = end_time - start_time
+  print("Done! Took {} seconds".format(elapsed_time))
 plt.show()
 
 
